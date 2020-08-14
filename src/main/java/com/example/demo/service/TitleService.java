a8f8fdda054d42fbcbdc0fbfd00ee58598c9ef1d
nagi <minami.nagisa.375@gmail.com>
2020-08-14 11:52:22 +0900

    JSON読込方法の変更

--- src/main/java/com/example/demo/service/TitleService.java
+++ src/main/java/com/example/demo/service/TitleService.java
@@ -1,5 +1,13 @@
 package com.example.demo.service;
 
+import java.io.BufferedOutputStream;
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.io.OutputStreamWriter;
+import java.net.HttpURLConnection;
+import java.net.URL;
+
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.beans.factory.annotation.Qualifier;
 import org.springframework.stereotype.Service;
@@ -20,5 +28,34 @@ public class TitleService {
 	public WorksDto service(String title) {
 		System.out.println("service呼び出し成功");
 		return restTemplate.getForObject(URL, WorksDto.class, title);
+	}
+
+	public String postJson(String json, String title) {
+		HttpURLConnection uc;
+		try {
+			URL url = new URL("https://api.annict.com/v1/works?filter_title=" + title
+					+ "&access_token=6TCOx_Yr4oe0vnENIq01CruyxtHMzh-is19txE8t5kM");
+			uc = (HttpURLConnection) url.openConnection();
+			uc.setRequestMethod("POST");
+			uc.setUseCaches(false);
+			uc.setDoOutput(true);
+			uc.setRequestProperty("Content-Type", "application/json; charset=utf-8");
+			OutputStreamWriter out = new OutputStreamWriter(new BufferedOutputStream(uc.getOutputStream()));
+			out.write(json);
+			out.close();
+
+			BufferedReader in = new BufferedReader(new InputStreamReader(uc.getInputStream()));
+			String line = in.readLine();
+			String body = "";
+			while (line != null) {
+				body = body + line;
+				line = in.readLine();
+			}
+			uc.disconnect();
+			return body;
+		} catch (IOException e) {
+			e.printStackTrace();
+			return "client - IOException : " + e.getMessage();
+		}
 	}
 }
