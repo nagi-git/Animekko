9e68fbf958c6e22adbefc8886fe68931f545df5d
nagi <minami.nagisa.375@gmail.com>
2020-08-17 17:50:04 +0900

    jacksonライブラリを使い、JSONの読込

--- src/main/java/com/example/demo/service/TitleService.java
+++ src/main/java/com/example/demo/service/TitleService.java
@@ -1,10 +1,7 @@
 package com.example.demo.service;
 
-import java.io.BufferedOutputStream;
 import java.io.BufferedReader;
-import java.io.IOException;
 import java.io.InputStreamReader;
-import java.io.OutputStreamWriter;
 import java.net.HttpURLConnection;
 import java.net.URL;
 
@@ -14,6 +11,10 @@ import org.springframework.stereotype.Service;
 import org.springframework.web.client.RestTemplate;
 
 import com.example.demo.dto.WorksDto;
+import com.fasterxml.jackson.core.JsonFactory;
+import com.fasterxml.jackson.core.JsonParser;
+import com.fasterxml.jackson.databind.JsonNode;
+import com.fasterxml.jackson.databind.ObjectMapper;
 
 @Service
 public class TitleService {
@@ -23,39 +24,46 @@ public class TitleService {
 	RestTemplate restTemplate;
 
 	/** アニメ検索API リクエストURL */
-	private static final String URL = "https://api.annict.com/v1/works?filter_title={title}&access_token=6TCOx_Yr4oe0vnENIq01CruyxtHMzh-is19txE8t5kM";
 
 	public WorksDto service(String title) {
 		System.out.println("service呼び出し成功");
+
+		String URL = "https://api.annict.com/v1/works?filter_title=" + title
+				+ "&access_token=6TCOx_Yr4oe0vnENIq01CruyxtHMzh-is19txE8t5kM";
+		getJsonNode(searchService(URL));
 		return restTemplate.getForObject(URL, WorksDto.class, title);
 	}
 
-	public String postJson(String json, String title) {
-		HttpURLConnection uc;
-		try {
-			URL url = new URL("https://api.annict.com/v1/works?filter_title=" + title
-					+ "&access_token=6TCOx_Yr4oe0vnENIq01CruyxtHMzh-is19txE8t5kM");
-			uc = (HttpURLConnection) url.openConnection();
-			uc.setRequestMethod("POST");
-			uc.setUseCaches(false);
-			uc.setDoOutput(true);
-			uc.setRequestProperty("Content-Type", "application/json; charset=utf-8");
-			OutputStreamWriter out = new OutputStreamWriter(new BufferedOutputStream(uc.getOutputStream()));
-			out.write(json);
-			out.close();
+	public String searchService(String urlStr) {
+		String result = "";
 
-			BufferedReader in = new BufferedReader(new InputStreamReader(uc.getInputStream()));
-			String line = in.readLine();
-			String body = "";
-			while (line != null) {
-				body = body + line;
-				line = in.readLine();
+		try {
+			URL url = new URL(urlStr);
+			HttpURLConnection con = (HttpURLConnection) url.openConnection();
+			con.connect();
+			BufferedReader in = new BufferedReader(new InputStreamReader(con.getInputStream()));
+			String tmp = "";
+			while ((tmp = in.readLine()) != null) {
+				result += tmp;
 			}
-			uc.disconnect();
-			return body;
-		} catch (IOException e) {
+			in.close();
+			con.disconnect();
+		} catch (Exception e) {
 			e.printStackTrace();
-			return "client - IOException : " + e.getMessage();
 		}
+		return result;
+	}
+
+	public static JsonNode getJsonNode(String jsonString) {
+		JsonNode head = null;
+		try {
+			JsonFactory jfactory = new JsonFactory();
+			JsonParser parser = jfactory.createJsonParser(jsonString);
+			ObjectMapper mapper = new ObjectMapper();
+			head = mapper.readTree(parser);
+		} catch (Exception e) {
+			e.printStackTrace();
+		}
+		return head;
 	}
 }
